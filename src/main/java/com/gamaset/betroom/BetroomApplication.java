46c5866ef063463c1a6c9f1664271755c3a116fc
Christopher S. Rozario <christopherscotini@gmail.com>
2018-01-08 15:47:28 -0200

    initial commit

---
+++ src/main/java/com/gamaset/betroom/BetroomApplication.java
@@ -0,0 +1,12 @@
+package com.gamaset.betroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetroomApplication.class, args);
+	}
+}
